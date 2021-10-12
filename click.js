5d9f09b24021023f039b71ce2d990d5234a6381e
dryelecoelho <90875948+dryelecoelho@users.noreply.github.com>
2021-10-12 12:23:15 -0300

    Add files via upload

---
+++ click.js
@@ -0,0 +1,8 @@
+function umClique (){
+	document.getElementById("imagem").src="open.jpg";
+	document.getElementById("log").innerHTML += "troca a foto do open<br>";
+}
+function doisCliques (){
+	document.getElementById("imagem").src="img.jpg";
+	document.gerElementById("log").innerHTML += "troca para a imagem<br>"
+}
